8edb54b8fdde82f5002fba11fea19b93eff229d1
dowright <dowright@nationofnobodies.com>
2021-08-29 00:34:21 -0700

    restructure and cleanup for potential release build.

--- dnx_nfqueue.pyx
+++ dnx_nfqueue.pyx
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
 
-'''
-Bind to a Linux netfilter queue. Send packets to a user-specified callback
-function.
-
-Copyright: (c) 2011, Kerkhoff Technologies Inc.
-License: MIT; see SOURCE-LICENSE.txt
-'''
-
-'''
-Expanded features + general refactor to be used with DNXFIREWALL.
- - DOWRIGHT @ Wright Network Solutions, LLC.
-'''
-
 import socket
+
+from libc.stdio cimport printf
 
 # Constants for module users
 cdef int COPY_NONE = 0
@@ -30,174 +19,119 @@ cdef u_int16_t MaxCopySize = 4096 - 80
 # formula: DEF_MAX_QUEUELEN * (MaxCopySize+SockOverhead) / 2
 cdef u_int32_t SockRcvSize = 1024 * 4796 // 2
 
-cdef int global_callback(nfq_q_handle * qh, nfgenmsg * nfmsg, nfq_data * nfa, void * data) with gil:
-    '''Create a Packet and pass it to appropriate callback.'''
+cdef object user_callback
+def set_user_callback(ref):
+    '''Set required reference which will be called after packet data is parsed into C structs.'''
+    global user_callback
 
-    cdef NetfilterQueue nfqueue = < NetfilterQueue > data
-    cdef object user_callback = < object > nfqueue.user_callback
+    user_callback = ref
 
-    packet = Packet()
-    packet.set_nfq_data(qh, nfa)
+cdef int nf_callback(nfq_q_handle *qh, nfgenmsg *nfmsg, nfq_data *nfa, void *data):
 
-    user_callback(packet)
+    cdef u_int32_t mark
+
+    packet = CPacket()
+    with nogil:
+        mark = packet.parse(qh, nfa)
+
+    user_callback(packet, mark)
 
     return 1
 
-cdef class Packet:
-    '''A packet received from NetfilterQueue.'''
+
+cdef class CPacket:
 
     def __cinit__(self):
         self._verdict_is_set = False
         self._mark = 0
-        self._given_payload = None
 
-    def __str__(self):
-        cdef ip_header * hdr = < ip_header * > self.payload
-        protocol = PROTOCOLS.get(hdr.protocol, "Unknown protocol")
-
-        return "%s packet, %s bytes" % (protocol, self.payload_len)
-
-    cdef set_nfq_data(self, nfq_q_handle * qh, nfq_data * nfa):
-        '''Assign a packet from NFQ to this object. Parse the header and load local values.'''
+    # NOTE: this will be callback target for nfqueue
+    cdef u_int32_t parse(self, nfq_q_handle *qh, nfq_data *nfa) nogil:
 
         self._qh = qh
         self._nfa = nfa
+
         self._hdr = nfq_get_msg_packet_hdr(nfa)
-
         self.id = ntohl(self._hdr.packet_id)
-        self.hw_protocol = ntohs(self._hdr.hw_protocol)
-        self.hook = self._hdr.hook
+        # NOTE: these are not needed at this moment.
+        # self.hw_protocol = ntohs(hdr.hw_protocol)
+        # self.hook = hdr.hook
 
-        self.payload_len = nfq_get_payload(self._nfa, &self.payload)
-        if self.payload_len < 0:
-            raise OSError("Failed to get payload of packet.")
-
-        self._payload
+        self.data_len = nfq_get_payload(self._nfa, &self.data)
+        # TODO: figure this out. cant use no gil if its here.
+        # if self.payload_len < 0:
+        #     raise OSError("Failed to get payload of packet.")
 
         # timestamp gets assigned via pointer/struct -> time_val: (t_sec, t_usec).
         nfq_get_timestamp(self._nfa, &self.timestamp)
 
-        cdef u_int32_t mark = nfq_get_nfmark(nfa)
+        self._mark = nfq_get_nfmark(nfa)
+
+        # splitting packet by tcp/ip layers
+        self._parse()
+
+        return self._mark
+
+        # if (self.continue_condition):
+        #     self._before_exit()
+
+    cdef void _parse(self) nogil:
+
+        self.ip_header = <iphdr*>self.data
+
+        cdef u_int8_t iphdr_len
+        cdef u_int8_t protohdr_len
+
+        iphdr_len = (self.ip_header.ver_ihl & 15) * 4
+
+        cdef unsigned char *_data = &self.data[iphdr_len]
+
+        if (self.ip_header.protocol == IPPROTO_TCP):
+            self.tcp_header = <tcphdr*>_data
+
+            protohdr_len = ((self.tcp_header.th_off >> 4) & 15) * 4
+
+        elif (self.ip_header.protocol == IPPROTO_UDP):
+            self.udp_header = <udphdr*>_data
+
+            protohdr_len = 8
+
+        elif (self.ip_header.protocol == IPPROTO_ICMP):
+            self.icmp_header = <icmphdr*>_data
+
+            protohdr_len = 4
+
+        self.cmbhdr_len = protohdr_len + 20
+
+        self.payload = &self.data[self.cmbhdr_len]
 
     cdef void verdict(self, u_int32_t verdict):
-        '''Call appropriate set_verdict... function on packet.'''
+        '''Call appropriate set_verdict function on packet.'''
 
+        # TODO: figure out what to do about this. maybe just printf instead?
         if self._verdict_is_set:
-            raise RuntimeWarning("Verdict already given for this packet.")
+            raise RuntimeWarning('Verdict already given for this packet.')
 
-        cdef u_int32_t modified_payload_len = 0
-        cdef unsigned char * modified_payload = NULL
-
-        # rewriting payload data/len
-        if self._given_payload:
-            modified_payload_len = len(self._given_payload)
-            modified_payload = self._given_payload
-
-        if self._modified_mark:
+        if self._mark:
             nfq_set_verdict2(
-                self._qh, self.id, verdict, self._modified_mark, modified_payload_len, modified_payload
+                self._qh, self.id, verdict, self._mark, self.data_len, self.data
             )
 
         else:
             nfq_set_verdict(
-                self._qh, self.id, verdict, modified_payload_len, modified_payload
+                self._qh, self.id, verdict, self.data_len, self.data
             )
 
         self._verdict_is_set = True
 
-    cpdef get_inint(self, bint name=False):
-        '''Returns index of inbound interface of packet. If the packet sourced from localhost or the input
-        interface is not known, 0 will be returned.
-        
-        if name=True, socket.if_indextoname() will be returned.
-        '''
-
-        cdef object in_interface_name
-
-        in_interface = nfq_get_indev(self._nfa)
-        if not name:
-            return in_interface
-
-        try:
-            in_interface_name = socket.if_indextoname(in_interface)
-        except OSError:
-            in_interface_name = 'unknown'
-
-        return in_interface_name
-
-    # NOTE: keeping these funtions separate instead of making an argument option to adjust which interface to return.
-    # this will keep it explicit for which interface is returning to minimize chance of confusion/bugs.
-    cpdef get_outint(self, bint name=False):
-        '''Returns index of outbound interface of packet. If the packet is destined for localhost or the output
-        interface is not yet known, 0 will be returned.
-        
-        if name=True, socket.if_indextoname() will be returned.
-        '''
-
-        cdef object out_interface_name
-
-        out_interface = nfq_get_outdev(self._nfa)
-        if not name:
-            return out_interface
-
-        try:
-            out_interface_name = socket.if_indextoname(out_interface)
-        except OSError:
-            out_interface_name = 'unknown'
-
-        return out_interface_name
-
-    def get_mark(self):
-        '''Return mark set at first receipt of packet.'''
-
-        return self._mark
-
-    cpdef update_mark(self, u_int32_t mark):
-        '''Modifies the running mark of the packet. This will not override the initial mark received, but any
-        subsequent update will override the previous.'''
-
-        self._modified_mark = mark
-
-    def get_hw(self):
-        '''Return the source mac address of the packet.'''
-
-        cdef object mac_addr
-
-        self._hw = nfq_get_packet_hw(self._nfa)
-        if self._hw == NULL:
-            # nfq_get_packet_hw doesn't work on OUTPUT and PREROUTING chains
-            # NOTE: making this a quick fail scenario since this would likely cause problems later in the packet
-            # parsing process and forcing error handling will ensure it is dealt with [properly].
-            raise OSError('MAC address not available in OUTPUT and PREROUTING chains')
-
-        # NOTE: can this not just be directly referenced below?
-        self.hw_addr = self._hw.hw_addr
-
-        mac_addr = PyBytes_FromStringAndSize( < char * > self.hw_addr, 8)
-
-        return mac_addr
-
-    def get_payload(self):
-        '''Return payload as Python string.'''
-
-        cdef object payload
-
-        payload = self.payload[:self.payload_len]
-
-        return payload
-
-    cpdef set_payload(self, bytes payload):
-        '''Set the new payload of this packet.'''
-
-        self._given_payload = payload
-
-    cpdef Py_ssize_t get_payload_len(self):
-
-        return self.payload_len
-
-    cpdef double get_timestamp(self):
+    cdef double get_timestamp(self):
 
         return self.timestamp.tv_sec + (self.timestamp.tv_usec / 1000000.0)
+
+    cpdef update_mark(self, u_int32_t mark):
+        '''Modifies the running mark of the packet.'''
+
+        self._mark = mark
 
     cpdef accept(self):
         '''Accept the packet.'''
@@ -222,6 +156,135 @@ cdef class Packet:
         '''Repeat the packet.'''
 
         self.verdict(NF_REPEAT)
+
+    def get_inint_name(self):
+
+        # cdef object *int_name
+        #
+        # nfq_get_indev_name(self)
+
+        pass
+
+    def get_outint_name(self):
+
+        # cdef object *int_name
+        #
+        # nfq_get_outdev_name(self)
+
+        pass
+
+    def get_hw(self):
+        '''Return hardware information of the packet.
+
+            hw_info = (
+                in_interface, out_interface, mac_addr, self.get_timestamp()
+            )
+        '''
+
+        cdef object mac_addr
+        cdef tuple hw_info
+        cdef int in_interface
+        cdef int out_interface
+
+        in_interface = nfq_get_indev(self._nfa)
+        out_interface = nfq_get_outdev(self._nfa)
+
+        self._hw = nfq_get_packet_hw(self._nfa)
+        if self._hw == NULL:
+            print(f'HW ERROR: {self._hw.hw_addr}')
+            # nfq_get_packet_hw doesn't work on OUTPUT and PREROUTING chains
+            # NOTE: making this a quick fail scenario since this would likely cause problems later in the packet
+            # parsing process and forcing error handling will ensure it is dealt with [properly].
+            raise OSError('MAC address not available in OUTPUT and PREROUTING chains')
+
+        cdef u_int8_t[8] hw_addr = self._hw.hw_addr
+
+        # NOTE: this is 8 bytes in source and lib_netfilter_queue, but unsure why since mac addresses are only 6
+        # bytes. the last two bytes may be padding, but either way removing here so it will not need to be done
+        # on the python side.
+        mac_addr = PyBytes_FromStringAndSize(<char*>hw_addr, 6)
+
+        hw_info = (
+            in_interface,
+            out_interface,
+            mac_addr,
+            self.get_timestamp(),
+        )
+
+        return hw_info
+
+    def get_raw_packet(self):
+        '''Return layer 3-7 of packet data.'''
+
+        return self.data[:self.data_len]
+
+    def get_ip_header(self):
+        '''Return layer3 of packet data as a tuple converted directly from C struct.'''
+
+        cdef tuple ip_header
+
+        ip_header = (
+            self.ip_header.ver_ihl,
+            self.ip_header.tos,
+            ntohs(self.ip_header.tot_len),
+            ntohs(self.ip_header.id),
+            ntohs(self.ip_header.frag_off),
+            self.ip_header.ttl,
+            self.ip_header.protocol,
+            ntohs(self.ip_header.check),
+            ntohl(self.ip_header.saddr),
+            ntohl(self.ip_header.daddr),
+        )
+
+        return ip_header
+
+    def get_proto_header(self):
+        '''Return layer4 of packet data as a tuple converted directly from C struct.'''
+
+        cdef tuple proto_header
+
+        if (self.ip_header.protocol == IPPROTO_TCP):
+
+            proto_header = (
+                ntohs(self.tcp_header.th_sport),
+                ntohs(self.tcp_header.th_dport),
+                ntohl(self.tcp_header.th_seq),
+                ntohl(self.tcp_header.th_ack),
+
+                self.tcp_header.th_off,
+
+                self.tcp_header.th_flags,
+                ntohs(self.tcp_header.th_win),
+                ntohs(self.tcp_header.th_sum),
+                ntohs(self.tcp_header.th_urp),
+            )
+
+        elif (self.ip_header.protocol == IPPROTO_UDP):
+
+            proto_header = (
+                ntohs(self.udp_header.uh_sport),
+                ntohs(self.udp_header.uh_dport),
+                ntohs(self.udp_header.uh_ulen),
+                ntohs(self.udp_header.uh_sum),
+            )
+
+        elif (self.ip_header.protocol == IPPROTO_ICMP):
+
+            proto_header = (
+                self.icmp_header.type,
+            )
+
+        else:
+            proto_header = ()
+
+        return proto_header
+
+    def get_payload(self):
+        '''Return payload (>layer4) as Python bytes.'''
+
+        cdef Py_ssize_t payload_len = self.data_len - self.cmbhdr_len
+
+        return self.payload[:payload_len]
 
 
 cdef class NetfilterQueue:
@@ -248,14 +311,13 @@ cdef class NetfilterQueue:
         # processes using this libnetfilter_queue on this protocol family!
         nfq_close(self.h)
 
-    def bind(self, int queue_num, object user_callback, u_int16_t max_len=DEFAULT_MAX_QUEUELEN,
+    def bind(self, int queue_num, u_int16_t max_len=DEFAULT_MAX_QUEUELEN,
             u_int8_t mode=NFQNL_COPY_PACKET, u_int16_t range=MaxPacketSize, u_int32_t sock_len=SockRcvSize):
         '''Create and bind to a new queue.'''
 
         cdef unsigned int newsiz
 
-        self.user_callback = user_callback
-        self.qh = nfq_create_queue(self.h, queue_num, < nfq_callback * > global_callback, < void * > self)
+        self.qh = nfq_create_queue(self.h, queue_num, <nfq_callback*>nf_callback, <void*>self)
         if self.qh == NULL:
             raise OSError(f'Failed to create queue {queue_num}')
 
@@ -291,15 +353,11 @@ cdef class NetfilterQueue:
         cdef int fd = self.get_fd()
         cdef char buf[4096]
         cdef int rv
-        cdef int recv_flags
-
-        recv_flags = 0
+        cdef int recv_flags = 0
 
         while True:
             with nogil:
                 rv = recv(fd, buf, sizeof(buf), recv_flags)
-
-            print(rv)
 
             if (rv >= 0):
                 nfq_handle_packet(self.h, buf, rv)
@@ -307,175 +365,3 @@ cdef class NetfilterQueue:
             else:
                 if errno != ENOBUFS:
                     break
-
-    def run_socket(self, s):
-        '''Accept packets using socket.recv so that, for example, gevent can monkeypatch it.'''
-
-        while True:
-            try:
-                buf = s.recv(4096)
-            except socket.error as e:
-                err = e.args[0]
-                if err == ENOBUFS:
-                    continue
-
-                elif err == EAGAIN or err == EWOULDBLOCK:
-                    # This should only happen with a non-blocking socket, and the
-                    # app should call run_socket again when more data is available.
-                    break
-
-                else:
-                    # This is bad. Let the caller handle it.
-                    raise e
-
-            else:
-                rv = len(buf)
-                if rv >= 0:
-                    nfq_handle_packet(self.h, buf, rv)
-                else:
-                    break
-
-PROTOCOLS = {
-    0: "HOPOPT",
-    1: "ICMP",
-    2: "IGMP",
-    3: "GGP",
-    4: "IP",
-    5: "ST",
-    6: "TCP",
-    7: "CBT",
-    8: "EGP",
-    9: "IGP",
-    10: "BBN-RCC-MON",
-    11: "NVP-II",
-    12: "PUP",
-    13: "ARGUS",
-    14: "EMCON",
-    15: "XNET",
-    16: "CHAOS",
-    17: "UDP",
-    18: "MUX",
-    19: "DCN-MEAS",
-    20: "HMP",
-    21: "PRM",
-    22: "XNS-IDP",
-    23: "TRUNK-1",
-    24: "TRUNK-2",
-    25: "LEAF-1",
-    26: "LEAF-2",
-    27: "RDP",
-    28: "IRTP",
-    29: "ISO-TP4",
-    30: "NETBLT",
-    31: "MFE-NSP",
-    32: "MERIT-INP",
-    33: "DCCP",
-    34: "3PC",
-    35: "IDPR",
-    36: "XTP",
-    37: "DDP",
-    38: "IDPR-CMTP",
-    39: "TP++",
-    40: "IL",
-    41: "IPv6",
-    42: "SDRP",
-    43: "IPv6-Route",
-    44: "IPv6-Frag",
-    45: "IDRP",
-    46: "RSVP",
-    47: "GRE",
-    48: "DSR",
-    49: "BNA",
-    50: "ESP",
-    51: "AH",
-    52: "I-NLSP",
-    53: "SWIPE",
-    54: "NARP",
-    55: "MOBILE",
-    56: "TLSP",
-    57: "SKIP",
-    58: "IPv6-ICMP",
-    59: "IPv6-NoNxt",
-    60: "IPv6-Opts",
-    61: "any host internal protocol",
-    62: "CFTP",
-    63: "any local network",
-    64: "SAT-EXPAK",
-    65: "KRYPTOLAN",
-    66: "RVD",
-    67: "IPPC",
-    68: "any distributed file system",
-    69: "SAT-MON",
-    70: "VISA",
-    71: "IPCV",
-    72: "CPNX",
-    73: "CPHB",
-    74: "WSN",
-    75: "PVP",
-    76: "BR-SAT-MON",
-    77: "SUN-ND",
-    78: "WB-MON",
-    79: "WB-EXPAK",
-    80: "ISO-IP",
-    81: "VMTP",
-    82: "SECURE-VMTP",
-    83: "VINES",
-    84: "TTP",
-    85: "NSFNET-IGP",
-    86: "DGP",
-    87: "TCF",
-    88: "EIGRP",
-    89: "OSPFIGP",
-    90: "Sprite-RPC",
-    91: "LARP",
-    92: "MTP",
-    93: "AX.25",
-    94: "IPIP",
-    95: "MICP",
-    96: "SCC-SP",
-    97: "ETHERIP",
-    98: "ENCAP",
-    99: "any private encryption scheme",
-    100: "GMTP",
-    101: "IFMP",
-    102: "PNNI",
-    103: "PIM",
-    104: "ARIS",
-    105: "SCPS",
-    106: "QNX",
-    107: "A/N",
-    108: "IPComp",
-    109: "SNP",
-    110: "Compaq-Peer",
-    111: "IPX-in-IP",
-    112: "VRRP",
-    113: "PGM",
-    114: "any 0-hop protocol",
-    115: "L2TP",
-    116: "DDX",
-    117: "IATP",
-    118: "STP",
-    119: "SRP",
-    120: "UTI",
-    121: "SMP",
-    122: "SM",
-    123: "PTP",
-    124: "ISIS",
-    125: "FIRE",
-    126: "CRTP",
-    127: "CRUDP",
-    128: "SSCOPMCE",
-    129: "IPLT",
-    130: "SPS",
-    131: "PIPE",
-    132: "SCTP",
-    133: "FC",
-    134: "RSVP-E2E-IGNORE",
-    135: "Mobility",
-    136: "UDPLite",
-    137: "MPLS-in-IP",
-    138: "manet",
-    139: "HIP",
-    140: "Shim6",
-    255: "Reserved",
-}
